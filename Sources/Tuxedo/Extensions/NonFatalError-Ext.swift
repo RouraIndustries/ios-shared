95302b0201e3d81dfa181099b9eab4343d879169
Christopher J. Roura <cjroura@gmail.com>
2024-03-28 01:00:36 -0400

    Color modification

--- Sources/Tuxedo/Extensions/NonFatalError-Ext.swift
+++ Sources/Tuxedo/Extensions/NonFatalError-Ext.swift
@@ -5,24 +5,10 @@
 //  Created by Christopher J. Roura on 2/7/24.
 //
 
-import Foundation
 import UIKit
 import RouraFoundation
 
 public extension NonFatalError {
-    static func couldNotSolveLineSpacingQuadraticEquation(for font: UIFont) -> NonFatalError {
-        let domain: String = "Could not determine line spacing"
-        let code: Int = -1
-        let userInfo: [String: Any] = [
-            NSLocalizedDescriptionKey: "Could not determine line spacing - solveQuadratic returned null",
-            "fontName": font.fontName,
-            "fontPointSize": font.pointSize,
-            "fontLineHeight": font.lineHeight
-        ]
-
-        return NonFatalError(domain: domain, code: code, userInfo: userInfo)
-    }
-
     static func unavailableFont(name: String) -> NonFatalError {
         let domain: String = "Font Style Unavailable - \(name)"
         let code: Int = 100
