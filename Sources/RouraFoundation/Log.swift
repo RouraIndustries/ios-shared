9876cca01c118d7ab214815d7b72db4b071fc596
Christopher J. Roura <cjroura@gmail.com>
2024-03-28 16:42:45 -0400

    Changed access to public for Log functions

--- Sources/RouraFoundation/Log.swift
+++ Sources/RouraFoundation/Log.swift
@@ -17,24 +17,24 @@ public enum Log {
 
     private static let subsystem = UIApplication.bundleId
 
-    static func log(_ level: LogLevel, _ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+    public static func log(_ level: LogLevel, _ message: String, file: String = #file, function: String = #function, line: Int = #line) {
         let logMessage = "\(level.rawValue) - [\(file):\(line)] \(function): \(message)"
         os_log("%{public}s", log: OSLog(subsystem: subsystem, category: level.rawValue), type: .default, logMessage)
     }
 
-    static func info(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+    public static func info(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
         log(.info, message, file: file, function: function, line: line)
     }
 
-    static func warn(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+    public static func warn(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
         log(.warning, message, file: file, function: function, line: line)
     }
 
-    static func error(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+    public static func error(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
         log(.error, message, file: file, function: function, line: line)
     }
 
-    static func nonfatal(_ error: NonFatalError, file: String = #file, function: String = #function, line: Int = #line) {
+    public static func nonfatal(_ error: NonFatalError, file: String = #file, function: String = #function, line: Int = #line) {
         log(.error, "Non-fatal error: \(error.localizedDescription)", file: file, function: function, line: line)
     }
 }
