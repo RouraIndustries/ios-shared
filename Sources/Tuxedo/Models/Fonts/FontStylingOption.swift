611dd389d8b92d18f086d56a33214e64f80ba981
Christopher J. Roura <60911272+elGordoRoura@users.noreply.github.com>
2024-06-08 12:19:33 -0400

    [MOBILE-5] Minor fix to apply FontStylingOption to fonts in SwiftUI only

--- Sources/Tuxedo/Models/Fonts/FontStylingOption.swift
+++ Sources/Tuxedo/Models/Fonts/FontStylingOption.swift
@@ -5,12 +5,27 @@
 //  Created by Christopher J. Roura on 6/7/24.
 //
 
-import Foundation
+import SwiftUI
 
 public enum FontStylingOption {
-    case small
+    case smallCaps
     case lowercaseSmallCaps
-    case uppercaseCaps
+    case uppercaseSmallCaps
     case italic
     case bold
+
+    /// Apply the styling option to the given font.
+    ///
+    /// - Parameters:
+    ///    - font: The font to apply the styling option.
+    /// - Returns: The modified font with the applied styling option.
+    func apply(to font: Font) -> Font {
+        switch self {
+        case .smallCaps:            return font.smallCaps()
+        case .lowercaseSmallCaps:   return font.lowercaseSmallCaps()
+        case .uppercaseSmallCaps:   return font.uppercaseSmallCaps()
+        case .italic:               return font.italic()
+        case .bold:                 return font.bold()
+        }
+    }
 }
